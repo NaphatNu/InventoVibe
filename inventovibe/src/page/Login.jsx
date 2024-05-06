0364b9aef24aeadaf1c5f2f0c5f4f252c7903211
NaphatNu <tonklanapat@gmail.com>
2024-05-07 02:32:58 +0700

    finish login

--- inventovibe/src/page/Login.jsx
+++ inventovibe/src/page/Login.jsx
@@ -3,37 +3,82 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'; 
 import { Navigate } from "react-router-dom";
 import './css/Login.css';
+import { getAllItems } from '../api/crud';
 
 function Login() {
   const [isLoggedIn, setIsLoggenIn] = useState(false);
 
-  function checkLogin() {
-    setIsLoggenIn(true);
+  const [Username, setUsername] = useState('');
+  const [Password, setPassword] = useState('');
+
+  const [Userdata, setUserdata] = useState([]);
+
+  const LoginData = {
+    Username,
+    Password,
   }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    checkLogin();
+
+    setUsername('');
+    setPassword('');
+    
+  }
+
+  async function checkLogin() {
+    const PasswordData = Userdata.map(item => item.Password)
+    const UsernameData = Userdata.map(item => item.Email)
+
+    for (let i = 0; i < Userdata.length; i++) {
+      if(PasswordData[i] === (LoginData.Password) && UsernameData[i] === (LoginData.Username)){
+        setIsLoggenIn(true)
+        console.log(true)
+        break
+      }
+    }
+  }
+
+  const fetchData = async () => {
+    try {
+        const response = await getAllItems('User');
+        console.log(response)
+        setUserdata(response);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+useEffect(() => {
+    fetchData();
+}, []);
+
 
   if(isLoggedIn){
     return <Navigate to="/home"/>;
   }
   else {
     return (
-      
       <div className="container">
         <div className="body">
           <div className="logo-container">
             <img src="./logo_InventoVibe2.PNG" alt="Logo" />
           </div>
           <div className="login-container">
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit}>
               <h2 className="login-title">Login</h2>
               <div className="form-group">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" className="form-control" />
+                <input type="email" id="username" className="form-control"
+                value={Username} onChange={(e) => setUsername(e.target.value)} />
               </div>
               <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" className="form-control" />
+                <input type="password" id="password" className="form-control"
+                value={Password} onChange={(e) => setPassword(e.target.value)} />
               </div>
-              <button type="submit" className="btn" onClick={checkLogin}>Login</button>
+              <button type="submit" className="btn" >Login</button>
             </form>
           </div>
           <div className="signup-link">
--- inventovibe/src/page/Signup.jsx
+++ inventovibe/src/page/Signup.jsx
@@ -1,9 +1,52 @@
 // Signup.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; // เรียกใช้ Link component จาก react-router-dom
 import './css/Signup.css'
+import { createItem } from '../api/crud';
 
 function SignUp() {
+
+  const [Name, setName] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Tel, setTel] = useState('');
+  const [Password, setPassword] = useState('');
+
+  const userData = {
+    Name,
+    Email,
+    Tel,
+    Password,
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('ส่งข้อมูลไปยัง API:', userData);
+    const res = createItem('User',userData);
+    console.log('submit : ', res);
+
+    setName('');
+    setEmail('');
+    setTel('');
+    setPassword('');
+    
+    if (isFormValid()) {
+      window.location.href = '/'; 
+    } else {
+      alert('Form is empty!');
+    }
+    
+  }
+
+  const isFormValid = () => {
+    // Check if any form field is empty
+    for (const key in userData) {
+      if (!userData[key]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
   return (
     <div className="body">
       <div className="signup-container">
@@ -11,24 +54,29 @@ function SignUp() {
             <img src="./logo_InventoVibe2.PNG" alt="Logo" />
           </div>
         <div className="signup-signup-container">
-          <form className="signup-form">
+          <form className="signup-form" onSubmit={handleSubmit}>
             
             <h2 className="signup-title">Sign Up</h2>
             <div className="signup-form-group">
               <label htmlFor="username">Name</label>
-              <input type="text" id="Name" className="signup-form-control" />
+              <input type="text" id="Name" className="signup-form-control" 
+              value={Name} onChange={(e) => setName(e.target.value)} />
+
             </div>
             <div className="signup-form-group">
               <label htmlFor="email">Email</label>
-              <input type="email" id="email" className="signup-form-control" />
+              <input type="email" id="email" className="signup-form-control"
+              value={Email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="signup-form-group">
               <label htmlFor="tel">Tel.</label>
-              <input type="number" id="tel" className="signup-form-control" />
+              <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" className="signup-form-control"
+              value={Tel} onChange={(e) => setTel(e.target.value)} />
             </div>
             <div className="signup-form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" className="signup-form-control" />
+              <input type="password" id="password" className="signup-form-control"
+              value={Password} onChange={(e) => setPassword(e.target.value)} />
             </div>
             <button type="submit" className="signup-btn">Sign Up</button>
           </form>

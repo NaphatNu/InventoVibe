2eb8eec98ca0c0c5ea6fa5842a15b10aeb3b4dc4
NaphatNu <tonklanapat@gmail.com>
2024-05-07 05:05:16 +0700

    final last!!

--- inventovibe/src/components/Navigation.jsx
+++ inventovibe/src/components/Navigation.jsx
@@ -1,18 +1,65 @@
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import './css/Navigation.css'
-import img from "../assets/logo_InventoVibe.png"
+import "./css/Navigation.css";
+import img from "../assets/logo_InventoVibe.png";
 
 function Navigation() {
-    return (
-        <div className="navigation-container">
-            <NavLink to="/home" className="Logo-container"><img className="Logo-img" src={img} alt="" /></NavLink>
-            <div className="navbar">
-                <NavLink to="/home" className="button">Stockpile</NavLink>   
-                <NavLink to="/History" className="button">History</NavLink>
-                <NavLink to="/Edit" className="button">Edit</NavLink>
-            </div>
-        </div>
-    );
+  const [selectedBox, setSelectedBox] = useState(
+    localStorage.getItem("selectedBox") || 'home'
+  );
+
+  const handleClick = (boxId) => {
+    setSelectedBox(boxId);
+    localStorage.setItem("selectedBox", boxId);
+  };
+
+  useEffect(() => {
+    // Add event listener to reset selected box on page unload
+    const handleUnload = () => {
+      localStorage.removeItem("selectedBox");
+    };
+    window.addEventListener("unload", handleUnload);
+    return () => {
+      window.removeEventListener("unload", handleUnload);
+    };
+  }, []);
+
+  return (
+    <div className="navigation-container">
+      <NavLink
+        to="/home"
+        className={`Logo-container ${selectedBox === "home" ? "" : ""}`}
+        onClick={() => handleClick("home")}
+      >
+        <img className="Logo-img" src={img} alt="" />
+      </NavLink>
+      <div className="navbar">
+        <NavLink
+          to="/home"
+          className={`button ${selectedBox === "home" ? "fill-color" : ""}`}
+          onClick={() => handleClick("home")}
+        >
+          Stockpile
+        </NavLink>
+        <NavLink
+          to="/History"
+          className={`button ${
+            selectedBox === "History" ? "fill-color" : ""
+          }`}
+          onClick={() => handleClick("History")}
+        >
+          History
+        </NavLink>
+        <NavLink
+          to="/Edit"
+          className={`button ${selectedBox === "Edit" ? "fill-color" : ""}`}
+          onClick={() => handleClick("Edit")}
+        >
+          Edit
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;

e47ef6cc9280f02787ce2b89a884ce9c3dbc5868
Pimmmvxg <129614165+Pimmmvxg@users.noreply.github.com>
2024-05-06 22:40:43 +0700

    History

--- inventovibe/src/page/History.jsx
+++ inventovibe/src/page/History.jsx
@@ -1,8 +1,57 @@
+import { useState, useEffect } from 'react';
+import './css/home.css';
+import List from '../components/list';
+import { getAllItems } from '../api/crud';
+
 function History() {
-    return (
-        <>
-        <div>History page</div>
-        </>
+
+        const ColumnList = ['ถุงซิป', 'แคป', 'ถุงแก้ว', 'pof', 'PVC'];
+        const [currentCategory, setCurrentCategory] = useState('ถุงซิป');
+        const [data, setData] = useState([]);
+    
+        // Function to handle category click
+        const handleClick = (category) => {
+            setCurrentCategory(category);
+        };
+    
+        // Function to fetch data from the API
+        const fetchData = async () => {
+            try {
+                const response = await getAllItems(currentCategory);
+                setData(response);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+    
+        // Fetch data initially and whenever the current category changes
+        useEffect(() => {
+            fetchData();
+    
+        }, [currentCategory]);
+    
+        return (
+            <div className='home-container'>
+                <div className='home-nav'>
+                    {ColumnList.map((item) => (
+                        <div
+                            key={item}
+                            className={`home-category ${currentCategory === item ? 'click' : ''}`}
+                            onClick={() => handleClick(item)}>
+                            {item}
+                        </div>
+                    ))}
+                </div>
+                <div className='home-content-container'>
+                    <div className='home-content-list'>
+                        <div className='item-list'>
+                            {data.map((item) => (
+                                <List key={item.id} itemData={item} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
     );
 }
 
